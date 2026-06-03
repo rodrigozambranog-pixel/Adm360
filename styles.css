a6b1bfcd5f65a1be6a2b2f2d6cab9cc200e682eb
rodrigozambranog-pixel <rodrigozambranog@gmail.com>
2026-06-03 10:37:23 -0400

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Adm360',
+  description: 'MVP de gestión de edificios, RRHH y operaciones'
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
